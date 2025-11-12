d73f9d1a31d04ba00a95673ddba11759bd258d3f
Zaminur <hasanzminur@gmail.com>
2025-11-12 07:10:50 +0600

    first commit with custom text style

---
+++ lib/app/view/style/text_style.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class TextStyles {
+
+  static Color color = Color(0xFF242B36);
+  
+  static TextStyle p1Reguler = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    color: color,
+  );
+  static TextStyle p1Medium = p1Reguler.copyWith(fontWeight: FontWeight.w500,);
+  static TextStyle p1SemiBold = p1Reguler.copyWith(fontWeight: FontWeight.w600,);
+
+  static TextStyle p2Reguler = p1Reguler.copyWith(fontSize: 18);
+  static TextStyle p2Medium = p2Reguler.copyWith(fontWeight: FontWeight.w500,);
+  static TextStyle p2SemiBold = p2Reguler.copyWith(fontWeight: FontWeight.w600,);
+
+  static TextStyle p3Reguler = p1Reguler.copyWith(fontSize: 20);
+  static TextStyle p3Medium = p3Reguler.copyWith(fontWeight: FontWeight.w500,);
+  static TextStyle p3SemiBold = p3Reguler.copyWith(fontWeight: FontWeight.w600,);
+}
---
+++ lib/feature/view/home_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    //print(TextStyle?.fontFamily);
+    return Scaffold(
+      body: Center(
+        child: Text("fgjaGGG",
+        //style: TextStyle(fontFamily: "Poppins",fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
